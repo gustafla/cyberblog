d1b67776fb374cf65a4cc9f468be969100343c17
Lauri Gustafsson <me@gustafla.space>
2021-12-02 16:04:30 +0200

    Add fields to post model

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 
 class Post(models.Model):
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    title = models.CharField(default="Post title", max_length=255)
     text = models.TextField()
     date = models.DateTimeField()
 
     def __str__(self):
-        return self.text
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Post
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the blog's index")
+    latest_posts = Post.objects.order_by("-date")[:10]
+    return HttpResponse(", ".join([str(p) for p in latest_posts]))
+
+
+def view(request, post_id):
+    return HttpResponse(f"You're looking at post {post_id}")
