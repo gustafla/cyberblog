56c186013b6ee0ec48d37e7f301307cdd9ace599
Lauri Gustafsson <me@gustafla.space>
2021-12-02 16:28:42 +0200

    Add post view page

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 
 from .models import Post
 
 
 def index(request):
-    latest_posts = Post.objects.order_by("-date")[:10]
+    latest_posts = Post.objects.order_by("-date")[:100]
     return render(request, "blog/index.html", context={"latest_posts": latest_posts})
 
 
 def view(request, post_id):
-    return HttpResponse(f"You're looking at post {post_id}")
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, "blog/view.html", {"post": post})
