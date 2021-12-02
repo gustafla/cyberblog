07c4574f3a5f375ffc5921b8cde21f1e137d9a73
Lauri Gustafsson <me@gustafla.space>
2021-12-02 16:53:10 +0200

    Add project index view

--- cyberblog/settings.py
+++ cyberblog/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "blog.apps.BlogConfig",
+    "cyberblog",
+    "blog",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
