68bdc1c488bbad7ef138880797ff63cb6c5b0d82
Lauri Gustafsson <me@gustafla.space>
2021-12-02 18:13:26 +0200

    Add user login page

---
+++ accounts/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as django_login
+
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user:
+            django_login(request, user)
+            return redirect(request.POST.get("next", "/"))
+        else:
+            return render(request, "accounts/login.html", {"invalid": True})
+
+    return render(
+        request, "accounts/login.html", {"next": request.GET.get("next", "/")}
+    )
--- cyberblog/urls.py
+++ cyberblog/urls.py
@@ -20,5 +20,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("blog/", include("blog.urls")),
+    path("accounts/", include("accounts.urls")),
     path("admin/", admin.site.urls),
 ]
